da35979f71a666cb7abea98a479293391cd8895d
Waleska Barros <parawaleska@gmail.com>
2014-09-10 09:40:36 -0300

    Atualizacao de classe

--- fj31-livraria/src/br/com/caelum/livraria/modelo/Livro.java
+++ fj31-livraria/src/br/com/caelum/livraria/modelo/Livro.java
@@ -4,9 +4,6 @@ import java.io.Serializable;
 
 public class Livro implements Serializable{
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	private String nome;
 	private double preco;
